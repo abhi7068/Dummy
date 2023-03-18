970fcf732e29d98096e86b9defe9d6d7acc0dc38
Hammad-4846 <curious4846@gmail.com>
2023-03-18 18:46:42 +0530

    First Commit

---
+++ tut1.js
@@ -0,0 +1,15 @@
+// var firstVar = "Aniket";
+var firstVar = "";
+
+//printing into terminal "Output"
+// console.log(firstVar);
+
+
+// if(firstVar){
+//     console.log("FirstVar is not Undefined")
+// }
+
+for(var i = 1; i <= 10 ; i++){
+    console.log(2 * i);
+}
+
---
+++ tut2.js
@@ -0,0 +1,16 @@
+//Scoping Diff b/w var & let or const
+// {
+//     // var i = 0;
+//     // var i = 1;
+
+//     let i = 0;
+
+//     const PI = 0;
+//     // PI = 1; Can not assign a new valut to constant variable 
+//     console.log(PI);
+// }
+
+
+const arr = ["firstValue","SecondValue",1,4.5,false];
+
+console.log(arr);
